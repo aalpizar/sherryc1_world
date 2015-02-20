9b49393d8dad40b7c5b608e1a449ea6c182cc110
Audrey Alpizar <audrey.alpizar@gmail.com>
2015-02-19 20:04:43 -0500

    added article model and updated category model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,11 @@
 class Article < ActiveRecord::Base
+	belongs_to :category
+	
+	#Validations
+	validates_presence_of :title, :content
+
+	#Scope
+	scope :active, where('active = ?', true)
+	scope :alphabetical, order('title')
+
 end
